4d7fb2c4204698f51d77eeb61b1542690113ac6a
ST\radek ripa <radek.ripa@st.com>
2020-02-25 10:41:20 +0100

    fisr working build

--- src/components/MainWindow.js
+++ src/components/MainWindow.js
@@ -1,8 +1,58 @@
 import React, { Component } from 'react';
 
+const lunchPlace = [
+  'Arab',
+  'Arabu u vody',
+  'Oliva',
+  'Vietnam - narohu',
+  'Vietnam - u autobusaku',
+  'Vietnam - nejstarsi',
+  'Vietnamske bagety',
+  'Pizza - LaFamilia',
+  'Pizza - Altia',
+  'Menza dole',
+  'Vegie garden + albert',
+  'Dhaba',
+  'Indie velka',
+  'Cina',
+  'Burgr',
+  'Reznik',
+  'Opera',
+  'Pivoklub',
+  'Karlinsky mlyn',
+  'Lahudky - autobusak',
+  'Co albert dal',
+  'Strelnice',
+  'Najit neco noveho'
+];
+
+function selectRandom(lenght) {
+  let randomValue = Math.floor(Math.random() * lenght);
+  console.log(randomValue);
+  return randomValue;
+}
 export class MainWindow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedPlace: '' };
+    this.selectRandomCallback = this.selectRandomCallback.bind(this);
+  }
+  selectRandomCallback() {
+    const selecterNumber = selectRandom(lunchPlace.length);
+    this.setState({ selectedPlace: lunchPlace[selecterNumber] });
+  }
+
   render() {
-    return <div>TEST</div>;
+    return (
+      <div>
+        <div>
+          <h1>{this.state.selectedPlace}</h1>
+        </div>
+        <div>
+          <button onClick={this.selectRandomCallback}>Select place</button>
+        </div>
+      </div>
+    );
   }
 }
 
